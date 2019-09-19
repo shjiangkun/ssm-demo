dd2e9f7666bb2fcdadb5cb0b9e7accd7a74e940b
CRIC-JK <405963004@qq.com>
2019-09-19 11:47:22 +0800

    test

--- src/com/how2java/controller/CategoryController.java
+++ src/com/how2java/controller/CategoryController.java
@@ -1,13 +1,17 @@
 package com.how2java.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.alibaba.fastjson.JSONObject;
 import com.how2java.util.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.how2java.pojo.Category;
@@ -20,7 +24,7 @@ public class CategoryController {
     @Autowired
     CategoryService categoryService;
 
-    @RequestMapping("listCategory")
+    @RequestMapping("/listCategory")
     public ModelAndView listCategory(Page page) {
         ModelAndView mav = new ModelAndView();
         PageHelper.offsetPage(page.getStart(), 5);
@@ -36,25 +40,25 @@ public class CategoryController {
         return mav;
     }
 
-    @RequestMapping("addCategory")
+    @RequestMapping("/addCategory")
     public ModelAndView addCategory(Category category) {
         ModelAndView mav = new ModelAndView();
         categoryService.add(category);
 
-        mav.setViewName("redirect:listCategory");
+        mav.setViewName("redirect:/listCategory");
         return mav;
     }
 
-    @RequestMapping("deleteCategory")
+    @RequestMapping("/deleteCategory")
     public ModelAndView deleteCategory(int id) {
         ModelAndView mav = new ModelAndView();
         categoryService.delete(id);
 
-        mav.setViewName("redirect:listCategory");
+        mav.setViewName("redirect:/listCategory");
         return mav;
     }
 
-    @RequestMapping("editCategory")
+    @RequestMapping("/editCategory")
     public ModelAndView editCategory(int id) {
         ModelAndView mav = new ModelAndView();
         Category c = categoryService.get(id);
@@ -63,12 +67,46 @@ public class CategoryController {
         return mav;
     }
 
-    @RequestMapping("updateCategory")
+    @RequestMapping("/updateCategory")
     public ModelAndView updateCategory(Category category) {
         ModelAndView mav = new ModelAndView();
         categoryService.update(category);
-        mav.setViewName("redirect:listCategory");
+        mav.setViewName("redirect:/listCategory");
         return mav;
     }
+
+    @ResponseBody
+    @RequestMapping("/submitCategory")
+    public String submitCategory(@RequestBody Category category) {
+        System.out.println("SSM接受到浏览器提交的json，并转换为Category对象:" + category);
+        return "ok";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/getOneCategory")
+    public String getOneCategory() {
+        Category c = new Category();
+        c.setId(100);
+        c.setName("第100个分类");
+        JSONObject json=new JSONObject();
+        json.put("category",JSONObject.toJSON(c));
+        return json.toJSONString();
+    }
+
+    @ResponseBody
+    @RequestMapping("/getManyCategory")
+    public String getManyCategory() {
+        List<Category> cs = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Category c = new Category();
+            c.setId(i);
+            c.setName("分类名称:"+i);
+            cs.add(c);
+        }
+
+        return JSONObject.toJSON(cs).toString();
+    }
+
 
 }
